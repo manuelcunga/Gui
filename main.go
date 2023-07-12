6f90f66fe01f13b6c248402f79793961edb6d160
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-07-12 20:12:17 +0100

    refatator bug fix

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	controller "github.com/manuelcunga/Gui/Gui/src/api/gui/controllers"
@@ -10,8 +11,8 @@ import (
 
 func main() {
 	e := echo.New()
-
-	generatorUsecase := usecase.NewOpenAIGeneratorUsecase(http.DefaultClient, "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")
+	token := os.Getenv("GPT_TOKEN")
+	generatorUsecase := usecase.NewOpenAIGeneratorUsecase(http.DefaultClient, token)
 
 	generateController := controller.NewGenerateController(generatorUsecase)
 
