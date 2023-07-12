8daac946081b3fedcba52d5a5010c9e71b1844c4
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-07-12 19:27:46 +0100

    fix bug 2

--- Gui/src/utils/utils.go
+++ Gui/src/utils/utils.go
@@ -8,16 +8,17 @@ import (
 
 func ParseBase64RequestData(request string) (string, error) {
 	dataBytes, err := base64.StdEncoding.DecodeString(request)
-
 	if err != nil {
 		return "", err
 	}
 
-	// content body
-
 	data, err := url.ParseQuery(string(dataBytes))
-	if data.Has("Body") {
-		return data.Get("Body"), nil
+	if err != nil {
+		return "", err
+	}
+
+	if body := data.Get("Body"); body != "" {
+		return body, nil
 	}
 
 	return "", errors.New("Body not found")
